67d7699d2fe216cbedda3a28c073c5a058931a52
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-09-13 11:39:08 +0600

    rev

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -7,7 +7,7 @@ const Category = ({ params }) => {
   return (
     <div>
       <h1 className='text-xl font-semibold text-green-400 '>{params.category}</h1>
-      <div className='mt-10 flex justify-between items-center rev '>
+      <div className='mt-10 flex justify-between items-center rev gap-20'>
         <div className='flex flex-col gap-5 flex-1'>
           <h1 className='text-2xl font-bold'>Test</h1>
           <p>des</p>
@@ -22,7 +22,37 @@ const Category = ({ params }) => {
             />
         </div>
       </div>
-      <div className='mt-10 flex justify-between items-center rev '>
+      <div className='mt-10 flex justify-between items-center rev gap-20'>
+        <div className='flex flex-col gap-5 flex-1'>
+          <h1 className='text-2xl font-bold'>Test</h1>
+          <p>des</p>
+          <button className='bg-green-700 px-5 py-3 text-sm rounded-md w-32'>See More</button>
+        </div>
+        <div className='flex relative h-[500px] flex-1'>
+        <Image
+              className="object-cover"
+              fill={true}
+              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+              alt=""
+            />
+        </div>
+      </div>
+      <div className='mt-10 flex justify-between items-center rev gap-20'>
+        <div className='flex flex-col gap-5 flex-1'>
+          <h1 className='text-2xl font-bold'>Test</h1>
+          <p>des</p>
+          <button className='bg-green-700 px-5 py-3 text-sm rounded-md w-32'>See More</button>
+        </div>
+        <div className='flex relative h-[500px] flex-1'>
+        <Image
+              className="object-cover"
+              fill={true}
+              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+              alt=""
+            />
+        </div>
+      </div>
+      <div className='mt-10 flex justify-between items-center rev gap-20'>
         <div className='flex flex-col gap-5 flex-1'>
           <h1 className='text-2xl font-bold'>Test</h1>
           <p>des</p>
