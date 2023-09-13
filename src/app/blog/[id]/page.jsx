f17a129e60240f431e79ea56ebe3f46516bcc838
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-09-13 13:37:50 +0600

    blog

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,11 +1,34 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Post = () => {
+const BlogPost = () => {
   return (
     <div>
-      Post
+      <div className='mt-10 flex justify-between items-center rev gap-20'>
+        <div className='flex flex-col gap-5 flex-1'>
+          <h1 className='text-2xl font-bold'>Test</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit quod reprehenderit quisquam dignissimos harum nobis debitis suscipit est laboriosam facilis eveniet voluptatibus, sapiente soluta maxime vero ut ducimus? Ullam, cumque!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, nisi saepe delectus dignissimos porro numquam ratione sed quaerat sint. Qui ipsum ab officia eum modi atque natus deserunt dolorem assumenda veritatis, est iusto non pariatur facilis officiis. Veritatis corrupti impedit nihil iusto perferendis! Optio quasi dolore repellat ab quia illo?
+          </p>
+        </div>
+        <div className='flex relative h-[500px] flex-1'>
+        <Image
+              className="object-cover"
+              fill={true}
+              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+              alt=""
+            />
+        </div>
+      </div>
+      <div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, quisquam!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore omnis non assumenda, suscipit ab, eaque minima corporis sequi odio magnam qui pariatur debitis nam exercitationem illum harum quod labore voluptatibus vero! Sint nesciunt odit ullam temporibus consectetur commodi facilis sunt fugit molestiae? Veniam voluptas nostrum optio ea dolorem, cupiditate non distinctio neque sit nulla laudantium odio eveniet exercitationem. Illo voluptatum eveniet consequatur repudiandae provident quasi deleniti, distinctio esse sit odio consequuntur dolor, adipisci officiis, recusandae fugit neque nobis aut eaque vel fugiat omnis! Suscipit quia quidem nemo corrupti odio saepe!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde modi deserunt rem fugit quos officiis? Error itaque ea reprehenderit ad soluta quod necessitatibus similique, quasi velit officiis dolorem blanditiis commodi. Expedita vero facilis alias possimus voluptate repellat earum! Quasi eligendi libero eveniet nihil, neque doloribus ipsam unde. Quas inventore dolorem in eius! Tempora nam id at saepe minima quibusdam fugit ut quisquam eligendi exercitationem quas nobis hic consequuntur totam delectus quae alias necessitatibus deserunt cumque error, atque beatae. Rerum explicabo natus odio. Numquam beatae magnam quaerat vero. Minima fugit qui eligendi veritatis enim, quasi dolore aspernatur sed animi labore, consectetur officiis neque nesciunt nam, porro nobis? Dolore porro soluta error eligendi numquam nulla id recusandae neque ad. Sapiente exercitationem delectus sunt earum expedita accusamus consectetur, ipsam fugiat sed sequi eligendi dolores quis veritatis hic numquam perferendis in commodi modi veniam reiciendis vel repudiandae quo. Cupiditate placeat beatae officia similique sit!</p>
+      </div>
     </div>
   )
 }
 
-export default Post
+export default BlogPost
