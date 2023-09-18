b68114c5e24a25dae49f9cd95d91591a3a6d101c
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-09-18 19:57:39 +0600

    models

---
+++ src/models/Post.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose
+
+const postSchema = new Schema({
+    name: {
+        title: string,
+        required: true,
+    },
+    desc: {
+        type: string,
+        required: true,
+    },
+    img: {
+        type: string,
+        required: true,
+    },
+    content: {
+        type: string,
+        required: true,
+    },
+    username: {
+        type: string,
+        required: true,
+    },
+}, { timestamps: true })
+
+export default mongoose.model("Post", postSchema)
