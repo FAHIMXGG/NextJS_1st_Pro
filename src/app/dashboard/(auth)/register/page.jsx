a547c8183da33d80a1bdede1f07190a302f81640
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-09-26 20:52:48 +0600

    reg page

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,11 +1,40 @@
 "use client"
 import Link from 'next/link'
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [err, setErr] = useState(false);
 
-  const handleSubmit = (e) =>{
+  const router = useRouter()
+
+  const handleSubmit = async (e) =>{
     e.preventDefault()
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    // const gg = ({email, password, name})
+    // console.log(gg)
+
+
+    try{
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password
+        }),
+      });
+
+      res.status === 201 && router
+
+    } catch (err) {
+      setErr(true)
+    }
   }
 
   return (
@@ -17,6 +46,7 @@ const Register = () => {
           <input type="password" placeholder='password' className='p-[10px] bg-transparent rounded-md border-[1px] border-gray-600 font-semibold ' required/>
           <button className='btn'>Register</button>
         </form>
+        {err && "Something went wrong"}
         <Link href="/dashboard/login" className='text-green-500'>Login with an existing account</Link>
       </div>
       
