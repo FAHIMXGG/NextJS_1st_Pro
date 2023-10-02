eca56af25a1b0e29b1c4eb5530b9665ee3b1243f
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-10-02 22:09:11 +0600

    dashoard design

--- src/app/dashboard/Page.jsx
+++ src/app/dashboard/Page.jsx
@@ -67,27 +67,29 @@ const Dashboard = () => {
     //console.log(data)
     if (session.status === "authenticated") {
         return (
-            <div>
+            <div className='flex  justify-between mt-20'>
                 {/* //post */}
-                <div>
+                <div className='w-1/2'>
                     {isLoading ? "loading" :
-                    data?.map((post) => (
-                        <div key={post._id}>
-                            <div>
-                                <Image src={post.img} width={200} height={150} alt="" ></Image>
+                        data?.map((post) => (
+                            <div key={post._id} className='flex items-center gap-10'>
+                                <div>
+                                    <Image src={post.img} width={200} height={150} alt="" ></Image>
+                                </div>
+                                <h2>{post.title}</h2>
+                                <span>X</span>
                             </div>
-                            <h2>{post.title}</h2>
-                            <span>X</span>
-                        </div>
-                    ))}
+                        ))}
                 </div>
-                <form action="" onSubmit={handleSubmit}>
-                    <h1>Add new Post</h1>
-                    <input type="text" placeholder='Title' />
-                    <input type="text" placeholder='Desc' />
-                    <input type="text" placeholder='Image' />
-                    <textarea placeholder='content' cols="30" rows="10"></textarea>
-                    <button>Post</button>
+                <form action="" onSubmit={handleSubmit} className='w-1/2 flex flex-col gap-1 '>
+                    <h1 className='text-center mb-5 text-green-500 font-semibold text-lg'>Add new Post </h1>
+                    <input className='p-1.5 rounded-sm bg-transparent border-[1px] border-gray-600 font-semibold  block focus:border-green-500 outline-none  placeholder:text-green-500 active:border-green-600' type="text" placeholder='Title' />
+                    <input className='p-1.5 rounded-sm bg-transparent border-[1px] border-gray-600 font-semibold  block focus:border-green-500 outline-none  placeholder:text-green-500 active:border-green-600' type="text" placeholder='Desc' />
+                    <input className='p-1.5 rounded-sm bg-transparent border-[1px] border-gray-600 font-semibold  block focus:border-green-500 outline-none  placeholder:text-green-500 active:border-green-600' type="text" placeholder='Image' />
+                    <textarea className='p-1.5 rounded-sm bg-transparent border-[1px] border-gray-600 font-semibold  block focus:border-green-500 outline-none  placeholder:text-green-500 active:border-green-600' placeholder='Content' cols="30" rows="10"></textarea>
+                    <div className='flex justify-center'>
+                        <button className='btn w-24'>Post</button>
+                    </div>
                 </form>
             </div>
         );
